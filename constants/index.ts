fac1c2bcc9f14b8fc8745c523ad8c01c6154ec42
AreebaZafarChohan <areebazafar715@gmail.com>
2025-06-15 17:47:33 +0500

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,360 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Bootstrap",
+    image: "bootstrap.png",
+    width: 110,
+    height: 110,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe",
+    image: "stripe.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Aceternity UI",
+    image: "aceternity-ui.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "GitHub",
+    icon: RxGithubLogo,
+    link: "https://github.com/AreebaZafarChohan",
+  },
+  {
+    name: "Linkedin",
+    icon: RxLinkedinLogo,
+    link: "https://www.linkedin.com/in/areeba-zafar-973917303/",
+  },
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://x.com/areebazafar715",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+   {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Bootstrap",
+    image: "bootstrap.png",
+    width: 110,
+    height: 110,
+  },
+  {
+    skill_name: "Aceternity UI",
+    image: "aceternity-ui.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Python",
+    image: "python.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "FastAPI",
+    image: "fastapi.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Gemini",
+    image: "gemini.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "DeepSeek",
+    image: "deepseek.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Openai",
+    image: "openai.png",
+    width: 100,
+    height: 100,
+  },
+  {
+    skill_name: "Pydantic",
+    image: "pydantic.png",
+    width: 70,
+    height: 70,
+  },
+] as const;
+
+export const FULLSTACK_SKILL = [
+  {
+    skill_name: "Next.Js",
+    image: "next.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Tailwind Css",
+    image: "tailwind.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    image: "figma.png",
+    width: 50,
+    height: 50,
+  },
+] as const;
+
+export const OTHER_SKILL = [
+  {
+    skill_name: "SDK Agent",
+    image: "sdk-agents.png",
+    width: 60,
+    height: 60,
+  },
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "Modern Next.js 14 Portfolio",
+    description:
+      'Embark on a journey through my professional evolution with the "Modern Next.js Portfolio" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by Next.js, this portfolio is more than just a static display; it\'s an immersive experience that reflects the cutting edge of modern web technologies.',
+    image: "/projects/project-1.png",
+    link: "https://my-portfolio-nextjs-brown.vercel.app/",
+  },
+  {
+    title: "Avion Furniture Marketplace",
+    description:
+      'Step into the refined world of modern commerce through "Avion Furniture" — a fully functional and aesthetically curated full-stack website that transforms the online furniture shopping experience. Moving beyond ordinary e-commerce designs, this project blends sleek UI with real-time features like dynamic product views, smart filtering, and secure checkout, all built with cutting-edge web technologies to deliver both elegance and efficiency.',
+    image: "/projects/project-2.png",
+    link: "https://avionfurnituremarketplace.vercel.app/",
+  },
+  {
+    title: "Tasty Treasure Bites",
+    description:
+      'Savor the flavor-packed journey with my "Tasty Treasure" food blog — a delightful digital space where culinary passion meets elegant design. Explore mouthwatering recipes, vibrant visuals, and rich storytelling that celebrate the art of food. This visually appetizing platform offers a warm, immersive experience crafted to inspire both food lovers and home chefs alike.',
+    image: "/projects/project-3.png",
+    link: "https://tasty-treasure.vercel.app/",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+      {
+        name: "YouTube",
+        icon: FaYoutube,
+        link: "https://youtube.com",
+      },
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/AreebaZafarChohan",
+      },
+      {
+        name: "Discord",
+        icon: RxDiscordLogo,
+        link: "https://discord.com",
+      },
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://instagram.com",
+      },
+      {
+        name: "Twitter",
+        icon: RxTwitterLogo,
+        link: "https://x.com/areebazafar715",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/areeba-zafar-973917303/",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+      {
+        name: "My Blogs",
+        icon: null,
+        link: "https://medium.com/@areebazafar715",
+      },
+      {
+        name: "Learning about me",
+        icon: null,
+        link: "https://github.com/AreebaZafarChohan",
+      },
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "mailto:areebazafar715@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "Home",
+    link: "#home",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+  {
+    title: "Contact Me",
+    link: "#contact",
+  }
+] as const;
+
+
